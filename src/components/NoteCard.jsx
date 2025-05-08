91c6128d2e4e8a8bfe4ba39c9184b41a41d1fbf8
Harsh <hharshrastogi@gmail.com>
2025-05-08 16:33:54 +0530

    Added Edit functionaltiy

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,11 +1,17 @@
-const NoteCard = ({note , onDelete }) => {
+const NoteCard = ({note , onDelete, onEdit }) => {
   return (
-    <div className="w-64 p-4 bg-white hover:shadow-xl transition-all duration-200 border rounded-2xl">
+    <div className="w-64 p-4 bg-gray-50 hover:shadow-xl transition-all duration-200 border-2 rounded-2xl">
       <h2 className="text-xl font-bold mb-2">{note.title}</h2>
       <p className="text-gray-700">{note.content}</p>
-      {/* <button>Edit</button> */}
+
       <button
-        className="mt-2 bg-red-500 text-white px-4 py-1 rounded"
+        className="mt-2 bg-green-500 text-white px-4 py-1 rounded"
+        onClick={() => onEdit(note)}
+      >
+        Edit
+      </button>
+      <button
+        className="mt-2 bg-red-500 text-white px-4 mx-2 py-1 rounded"
         onClick={() => onDelete(note.id)}
       >
         Delete
--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,46 +1,70 @@
 import { useState, useEffect } from "react";
 import NavBar from "../components/Navbar";
-import NoteCard from "../components/NoteCard"
+import NoteCard from "../components/NoteCard";
 
 const Notes = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [notes, setNotes] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [currentNote, setCurrentNote] = useState(null);
 
   const handlesubmit = (e) => {
     e.preventDefault();
     if (!title.trim() || !content.trim()) return;
 
-    const newNote = {
-      id: Date.now(), title, content
-    };
-    setNotes([...notes, newNote]);
+    if (editMode) {
+      const updatednotes = notes.map((note) =>
+        note.id === currentNote.id ? { ...note, title, content } : note
+      );
+      setNotes(updatednotes);
+      setEditMode(false);
+      setCurrentNote(null);
+    } else {
+      const newNote = {
+        id: Date.now(),
+        title,
+        content,
+      };
+      setNotes([...notes, newNote]);
+    }
+
     setTitle("");
     setContent("");
   };
 
-  const handleDelete = (id) =>{
-    const updatednotes = notes.filter((note) => note.id !== id)
-    setNotes(updatednotes)
-  }
+  const handleDelete = (id) => {
+    const updatednotes = notes.filter((note) => note.id !== id);
+    setNotes(updatednotes);
+  };
+
+  const handleEdit = (note) => {
+    setTitle(note.title);
+    setContent(note.content);
+    setEditMode(true);
+    setCurrentNote(note);
+  };
 
   useEffect(() => {
-    localStorage.setItem('notes',JSON.stringify(notes))
-  },[notes]);
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
 
-  useEffect(()=>{
-    const storedNotes = localStorage.getItem('notes');
-    if(storedNotes){
-      setNotes(JSON.parse(storedNotes))
+  useEffect(() => {
+    const storedNotes = localStorage.getItem("notes");
+    if (storedNotes) {
+      setNotes(JSON.parse(storedNotes));
     }
-  },[])
+  }, []);
   return (
     <>
       <NavBar />
       <div className="">
         <form onSubmit={handlesubmit}>
           <div className="p-6 max-w-xl mx-auto">
-            <h1 className="text-3xl font-bold mb-4">Add Your Notes Here:</h1>
+            <h1 className="text-3xl font-bold mb-4">
+              {editMode ? "Edit Your Note:" : "Add Your Notes Here:"}
+            </h1>
+
             <label className="p-3 font-bold">Title:</label>
             <input
               type="text"
@@ -58,18 +82,24 @@ const Notes = () => {
             />
             <button
               type="submit"
-              className="bg-blue-500 text-white px-4 py-2 rounded mb-6"
+              className="bg-transparent hover:bg-black text-black font-semibold hover:text-white py-2 px-4 border border-black hover:border-transparent rounded"
             >
-              Add Note
+              {editMode ? "Save Changes" : "Add Note"}
             </button>
 
-            <div className="flex flex-wrap gap-4">
-              {notes.map((note) => (
-                <NoteCard key={note.id} note={note} onDelete={handleDelete} />
-          ))}
-              </div>
+            
           </div>
         </form>
+        <div className="flex flex-wrap gap-4 mx-4">
+              {notes.map((note) => (
+                <NoteCard
+                  key={note.id}
+                  note={note}
+                  onDelete={handleDelete}
+                  onEdit={handleEdit}
+                />
+              ))}
+            </div>
       </div>
     </>
   );
